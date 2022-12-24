4074ceca5dbc0a80a48db97d508e034334e29b75
muratavci05 <js.muratavci@gmail.com>
2022-12-25 02:58:56 +0300

    home page modified

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from "react";
 import "./home.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
 
 const Home = () => {
 
-
+const [popularMovies, setPopularMovies]=useState([]);
 useEffect(()=>{
     fetch("https://api.themoviedb.org/3/movie/popular?api_key=fe14e7e0569e940a0ff967d375af6744&language=en-US")
     .then(res=>res.json())
-    .then(data=>console.log(data.results))
-},[])
+    .then(data=>setPopularMovies(data.results))
+},[]);
 
     return(
         <React.Fragment>
-            Home Page is now rendered
+            <div className="poster">
+                <Carousel
+                    showThumbs={false}
+                    autoPlay={true}
+                    transitionTime={3}
+                    infiniteLoop={true}
+                    showStatus={false}
+                >
+                    {
+                        popularMovies.map(movie =>(
+                            <div className="posterImage">
+                                <img src="" />
+
+                            </div>
+                        ))
+                    }
+
+                </Carousel>
+            </div>
          </React.Fragment>
 
     )
