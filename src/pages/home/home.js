a97f2f1ddd334600996bcd9aecafc7532ae2ed54
muratavci05 <js.muratavci@gmail.com>
2022-12-25 02:26:08 +0300

    home.js api add and edit

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 
 const Home = () => {
 
-    
+
+useEffect(()=>{
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=fe14e7e0569e940a0ff967d375af6744&language=en-US")
+    .then(res=>res.json())
+    .then(data=>console.log(data.results))
+},[])
+
     return(
         <React.Fragment>
             Home Page is now rendered
