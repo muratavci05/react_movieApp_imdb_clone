047f32d41960374945ba59639e07c50bd54389fd
muratavci05 <js.muratavci@gmail.com>
2022-12-25 03:17:11 +0300

    homejs modified<title,description etc>

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
@@ -24,12 +25,30 @@ useEffect(()=>{
                 >
                     {
                         popularMovies.map(movie =>(
+                            <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`}>
                             <div className="posterImage">
-                                <img src="" />
+                                <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
 
                             </div>
+                            <div className="posterImage__overlay">
+                                <div className="posterImage__title">
+                                    {movie ? movie.original_title: ""}
+                                </div>
+                                <div className="posterImage__runtime">
+                                    {movie ? movie.release_date : ""}
+                                    <span className="posterImage__rating">
+                                        {movie ? movie.vote_average : ""}
+                                        <i className="fas fa-star"/>{""}
+                                    </span>
+                                </div>
+                                <div className="posterImage__description">
+                                    {movie ? movie.overview :""}
+                                </div>
+
+                            </div>
+                            </Link>
                         ))
-                    }
+                    };
 
                 </Carousel>
             </div>
